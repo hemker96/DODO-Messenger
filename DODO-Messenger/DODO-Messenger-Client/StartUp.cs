e85eaa8b827c5d80960ba4975e7c5dd6ac7b6f4c
wlanhunter <florian.widder@live.de>
2015-11-11 18:27:41 +0100

    Grundprojekt

---
+++ DODO-Messenger/DODO-Messenger-Client/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DODO_Messenger_Client
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StartUp Client");
+            Console.ReadKey();
+        }
+    }
+}
