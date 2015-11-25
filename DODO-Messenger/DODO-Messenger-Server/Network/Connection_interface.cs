02e1935d33e4c6116bca00c2df303f56da299345
wlanhunter <florian.widder@live.de>
2015-11-25 12:11:18 +0100

    Test

---
+++ DODO-Messenger/DODO-Messenger-Server/Network/Connection_interface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DODO_Messenger_Server.Network
+{
+    abstract class Connection_interface
+    {
+        public abstract void ClientBinder();
+        public abstract void send(String toSend);
+    }
+}
